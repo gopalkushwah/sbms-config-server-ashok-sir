d44b8ea5a04cfd6b84a8b602a55e762643567a17
gopalkushwah <gopalkushwah15101999@gmail.com>
2024-07-07 15:09:40 +0530

    Welcome  Config Client

---
+++ src/main/java/com/welcomeconfigclient/SbmsWelcomeApiConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.welcomeconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsWelcomeApiConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsWelcomeApiConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welcomeconfigclient/SbmsWelcomeApiConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcomeconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsWelcomeApiConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
